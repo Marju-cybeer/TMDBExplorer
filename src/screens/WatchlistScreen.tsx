282647115c6e5e1a108f7d290ab4a4cb6fe3f898
marju-cybeer <martha.1958@aluno.pr.senac.br>
2026-01-07 21:56:14 -0300

    erro de hoocks

--- src/screens/WatchlistScreen.tsx
+++ src/screens/WatchlistScreen.tsx
@@ -1,15 +1,24 @@
-import { FlatList } from "react-native";
+import { FlatList, View, ActivityIndicator } from "react-native";
 import { useFavorites } from "../hooks/useFavorites";
 import { MovieCard } from "../components/MovieCard";
 
 export default function WatchlistScreen() {
-  const { favorites } = useFavorites();
+  const { favorites, loading } = useFavorites();
+console.log(favorites)
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <FlatList
       data={favorites}
       keyExtractor={(item) => item.id.toString()}
       numColumns={3}
+      contentContainerStyle={{ padding: 8 }}
       renderItem={({ item }) => (
         <MovieCard
           movie={{
@@ -18,10 +27,10 @@ export default function WatchlistScreen() {
             poster_path: item.poster,
             vote_average: item.rating,
             release_date: item.releaseDate,
+            backdrop_path: undefined, // ✅ evita erro de tipagem
           }}
         />
       )}
     />
   );
 }
-
