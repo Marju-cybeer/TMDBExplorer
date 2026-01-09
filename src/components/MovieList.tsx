10944d5b1a8e0a86449efc77e60be156aed6a098
marju-cybeer <martha.1958@aluno.pr.senac.br>
2026-01-09 20:15:48 -0300

    Alterando posição de lista de apresentação de cards

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,7 +2,7 @@ import { FlatList } from "react-native";
 import { Movie } from "../types/movie";
 import { MovieCard } from "./MovieCard";
 import { useThemeStyles } from "../theme/useThemeStyles";
-import React from "react";
+import React, { useCallback } from "react";
 
 interface Props {
   movies: Movie[];
@@ -17,22 +17,40 @@ export function MovieList({
 }: Props) {
   const { spacing } = useThemeStyles();
 
-  return (
-    <FlatList
-      data={movies}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <MovieCard
-          movie={item}
-          onPress={() => onPressMovie?.(item)}
-        />
-      )}
-      ListHeaderComponent={ListHeaderComponent}
-      contentContainerStyle={{
-        paddingHorizontal: spacing.md,
-        paddingBottom: spacing.xl,
-      }}
-      showsVerticalScrollIndicator={false}
-    />
+  const renderItem = useCallback(
+    ({ item }: { item: Movie }) => (
+      <MovieCard
+        movie={item}
+        onPress={() => onPressMovie?.(item)}
+      />
+    ),
+    [onPressMovie]
   );
+
+  return (
+  <FlatList
+    data={movies}
+    renderItem={renderItem}
+    keyExtractor={(item) => item.id.toString()}
+    ListHeaderComponent={ListHeaderComponent}
+
+    numColumns={3}
+    columnWrapperStyle={{
+      justifyContent: "space-between",
+      marginBottom: spacing.md,
+    }}
+
+    initialNumToRender={10}
+    maxToRenderPerBatch={10}
+    windowSize={5}
+    removeClippedSubviews
+
+    contentContainerStyle={{
+      paddingHorizontal: spacing.md,
+      paddingBottom: spacing.xl,
+    }}
+    showsVerticalScrollIndicator={false}
+  />
+);
+
 }
