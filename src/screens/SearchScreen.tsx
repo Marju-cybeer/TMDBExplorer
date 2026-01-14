66f25cd53890057e974be512cb6e8e8e8d8ef9fd
marju-cybeer <martha.1958@aluno.pr.senac.br>
2026-01-14 20:16:29 -0300

    ajustes de pesquisa na home

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,26 +1,39 @@
 import { useState } from "react";
 import { FlatList, View, TouchableOpacity } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
 import { SearchInput } from "../components/SearchInput";
 import { useSearchMovies } from "../hooks/useSearchMovies";
 import { MovieCard } from "../components/MovieCard";
-import { useNavigation } from "@react-navigation/native";
 
 export default function SearchScreen() {
-  const [query, setQuery] = useState("");
+  const navigation = useNavigation<any>();
+  const route = useRoute<any>();
+
+  // 🔥 recebe texto vindo da Home (se existir)
+  const [query, setQuery] = useState(
+    route.params?.initialQuery ?? ""
+  );
+
   const { movies } = useSearchMovies(query);
-  const navigation: any = useNavigation();
 
   return (
     <View style={{ flex: 1, padding: 16 }}>
-      <SearchInput value={query} onChangeText={setQuery} />
+      {/* ✅ PROP CORRETA */}
+      <SearchInput
+        value={query}
+        onChangeText={setQuery}
+      />
 
       <FlatList
         data={movies}
         keyExtractor={(item) => item.id.toString()}
         numColumns={3}
         contentContainerStyle={{ marginTop: 16 }}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <TouchableOpacity
+            activeOpacity={0.8}
             onPress={() =>
               navigation.navigate("MovieDetails", {
                 movieId: item.id,
