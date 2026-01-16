1746a96f6db3cdc74e794cff58f8e4c6dbe3a731
Marju-cyber <marthajhermann@gmail.com>
2026-01-16 16:26:16 -0300

    Ajustes para o login funcionar

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 import { RootNavigator } from "./src/navigation/RootNavigator";
 import { AuthProvider } from "./src/context/AuthContext";
 import { ThemeProvider } from "./src/store/theme.store";
 import { useEffect } from "react";
 import { initDatabase } from "./src/database";
 
-useEffect(() => {
-  initDatabase();
-}, []);
-
-
-
 export default function App() {
+  useEffect(() => {
+    initDatabase();
+  }, []);
+
   return (
     <AuthProvider>
       <ThemeProvider>
         <RootNavigator />
+        <StatusBar style="auto" />
       </ThemeProvider>
     </AuthProvider>
   );
